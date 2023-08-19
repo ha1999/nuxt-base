bf419452b2edd1cdd6ae54629fc96a99a63f24f0
haico <haico>
2023-08-19 16:56:25 +0700

    feat: init project base of nuxtjs app

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  typescript: {
+    strict: true,
+  },
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  runtimeConfig: {},
+  nitro: {},
+  vite: {},
+  webpack: {},
+  postcss: {},
+});
