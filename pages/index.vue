4d360bff773123fca3253ced63084ffe819dea58
Lê Ngọc Hà (Haico) <43954353+ha1999@users.noreply.github.com>
2023-08-20 19:42:51 +0700

    feat: add meta seo
    Co-authored-by: haico <haico>

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { data: image } = await useFetch(
+  `https://api.slingacademy.com/v1/sample-data/photos/${Math.floor(
+    Math.random() * 100 + 1,
+  )}`,
+);
+
+useSeoMeta({
+  title: () => image?.photo?.title,
+  ogTitle: () => image?.photo?.title,
+  description: () => image?.photo?.description,
+  ogDescription: () => image?.photo?.description,
+  ogImage: () => image?.photo?.url,
+  twitterCard: () => image?.photo?.description,
+});
+</script>
